373fd815692d89ff0ed4413a5afd0d653f5347c7
Francisco Fonseca <franfonseca8594@gmail.com>
2024-03-05 21:50:22 -0800

    added pages and components

--- franfonse.com-frontend/src/app/app.component.ts
+++ franfonse.com-frontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+
+// ROUTER
 import { RouterOutlet } from '@angular/router';
+
+// COMPONENTS
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- franfonse.com-frontend/src/app/app.routes.ts
+++ franfonse.com-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component: HomeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'not-found', component: NotFoundComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'not-found' },
+];
